4058c31968249dfed319ce777a4dc0b891b3d6a1
Jesslyn Landgren <jesslyn.landgren@gmail.com>
2016-11-01 10:22:13 -0400

    Finished function findWinner

--- tictactoe-practice.js
+++ tictactoe-practice.js
@@ -1,19 +1,28 @@
-function check(a, b){
-    return a == this;
+
+function findWinner(a) {
+    var winner =  a.map(function(b){
+        if (b.every(function(c) {return c == this;},b[0]) === true){
+            return b[0];
+        } else {
+            return null;
+        }
+    }).find(function(d) {
+        return d !== null;
+    });
+    if (winner !== 'O' && winner !== 'X'){
+        return null;
+    } else {
+        return winner;
+    }
 }
 
-function checkBoard(a) {
-    return a.map(function(c) {
-        return c.every(check,c[0]);
-        });
-}
-
-row = ['O', 'O', '0'];
-
-console.log(row.every(check, row[0]));
-
-board = [['O', 'O', 'O'],
+board1 = [['O', 'O', 'O'],
          ['X', null, 'X'],
          [null, 'X', null]];
 
-console.log(checkBoard(board));
+board2 = [['null', 'null', 'null'],
+      ['X', null, 'X'],
+      [null, 'X', null]];
+
+console.log(findWinner(board1));
+console.log(findWinner(board2));
